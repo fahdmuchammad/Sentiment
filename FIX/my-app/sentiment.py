98aec9f2949b9b4d8f7955f66825ff55df017e13
Fahd Muchammad <fahdmuchammad@Fahds-MacBook-Pro.local>
2023-06-18 22:18:27 +0700

    updatebaru banget pisanbaru2

--- FIX/my-app/sentiment.py
+++ FIX/my-app/sentiment.py
@@ -3,8 +3,8 @@ import re
 import string
 import pickle
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory #stopword remover
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory #stemming
 
